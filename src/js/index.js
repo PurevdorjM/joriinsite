81612c2fdec2e73871cbf2bd35ce0f8add64b223
puujeest <puujeest@gmail.com>
2020-05-09 15:25:13 +0800

    Haih tovchluur Saerch controller

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,32 @@
+require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
-search.doSearch().then((r) => console.log(r));
+/**
+ * Web app төлөв
+ * -Хайлтын query, үр дүн
+ * -Тухайн үзүүлж байгаа код
+ * -Лайкаслан жорууд
+ * -Захиалж байгаа жорын найрлагууд
+ */
+
+const state = {};
+const controlSearch = async () => {
+  //  1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна
+  const query = "pizza";
+  if (query) {
+    //  2) Шинээр хайлтын объектийг үүсгэж өгнө.
+    state.search = new Search(query);
+
+    //  3) Хайлт хийхэд зориулж дэлгэцийн UI бэлдэж өгнө.
+
+    //   4) Хайлтыг гүйцэтгэнэ
+    await state.search.doSearch();
+    //   5) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+    console.log(state.search.result);
+  }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
