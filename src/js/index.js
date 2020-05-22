2913bac8015705d4a87d373a5b73176f368f908e
puujeest <puujeest@gmail.com>
2020-05-22 18:04:07 +0800

    Sagsnii model hiisen

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,13 @@ import Search from "./model/Search";
 import { elements, renderLoader, clearLoader } from "./view/base";
 import * as searchView from "./view/searchView";
 import Recipe from "./model/Recipe";
+import List from "./model/list";
+import * as listView from "./view/listView";
+import {
+  renderRecipe,
+  clearRecipe,
+  highLightSelectedRecipe,
+} from "./view/recipeView";
 
 /**
  * Web app төлөв
@@ -13,6 +20,12 @@ import Recipe from "./model/Recipe";
  */
 
 const state = {};
+
+/**
+ * Хайлтын контроллер - MVC Model ==> Controller <== View
+ *
+ */
+
 const controlSearch = async () => {
   //  1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна
   const query = searchView.getInput();
@@ -49,5 +62,63 @@ elements.pageButtons.addEventListener("click", (e) => {
   }
 });
 
-const r = new Recipe(47746);
-r.getRecipe();
+/**
+ * Жорын контролллер
+ */
+
+const controlRecipe = async () => {
+  // 1) URL -аас ID-г салгаж авна
+  const id = window.location.hash.replace("#", "");
+
+  // URL дээр ID байгаа эсэхийг шалгана.
+  if (id) {
+    // 2) Жорын моделийг үүсгэж өгнө.
+    state.recipe = new Recipe(id);
+
+    // 3) UI дэлгэцийг бэлтгэнэ.
+    clearRecipe();
+    renderLoader(elements.recipeDiv);
+    highLightSelectedRecipe(id);
+
+    // 4) Жороо татаж авна.
+    await state.recipe.getRecipe();
+
+    // 5) Жорыг гүйцэтгэх хугацаа болон орцыг тооцоолно.
+    clearLoader();
+    state.recipe.calcTime();
+    state.recipe.calcHuniiToo();
+    // 6) Жороо дэлгэцэнд гаргана.
+    renderRecipe(state.recipe);
+  }
+};
+// window.addEventListener("hashchange", controlRecipe);
+// window.addEventListener("load", controlRecipe);
+
+["hashchange", "load"].forEach((e) =>
+  window.addEventListener(e, controlRecipe)
+);
+
+/**
+ * Найрлаганы контролллер
+ */
+
+const controlList = () => {
+  // Найрлаганы мопелийг үүсгэнэ.
+  state.list = new List();
+  // Өмнө нь байсан мэдээллүүдийг арилгах
+  listView.clearItems();
+
+  // Уг моделруу одоо харагдаж байгаа жорны бүх найрлагыг авч хийнэ.
+  state.recipe.ingredients.forEach((n) => {
+    // Тухайн найрлагыг моделруу хийнэ
+    state.list.addItem(n);
+    // Тухайн найрлагыг дэлгэцэнд гаргана
+    listView.renderItem(n);
+  });
+};
+
+elements.recipeDiv.addEventListener("click", (e) => {
+  if (e.target.matches(".recipe__btn, .recipe__btn *")) {
+    controlList();
+  }
+});
--- src/js/view/searchView.js
+++ src/js/view/searchView.js
@@ -10,7 +10,6 @@ import { elements } from "./base";
 
 // private function
 const renderRecipe = (recipe) => {
-  console.log(recipe);
   const markup = `<li>
   <a class="results__link" href="#${recipe.recipe_id}">
       <figure class="results__fig">
